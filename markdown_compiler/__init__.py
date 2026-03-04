24d27a6960af814feb34aaac41a844bec4f5e725
Apple <apple@MacBook-Air-2619.local>
2026-03-03 21:26:52 -0800

    markdown_compiler/__init__.py

--- markdown_compiler/__init__.py
+++ markdown_compiler/__init__.py
@@ -3,7 +3,17 @@ This file contains functions that work on entire documents at a time
 (and not line-by-line).
 '''
 
-from markdown_compiler.util.line_functions import *
+from markdown_compiler.util.line_functions import (
+    compile_headers,
+    compile_strikethrough,
+    compile_bold_stars,
+    compile_bold_underscore,
+    compile_italic_star,
+    compile_italic_underscore,
+    compile_code_inline,
+    compile_images,
+    compile_links,
+)
 
 
 def compile_lines(text):
@@ -20,55 +30,90 @@ def compile_lines(text):
     The first set of doctests below show how this function adds <p> tags and calls the functions above.
     Once you implement the functions above correctly,
     then this first set of doctests will pass.
-
-    NOTE:
-    For your assignment, the most important thing to take away from these test cases is how multiline tests can be formatted
     '''
+
     lines = text.split('\n')
     new_lines = []
-
     in_paragraph = False
-    in_code = False
-
+    in_pre = False
     for line in lines:
-        stripped = line.strip()
-        if stripped == "```":
-            if in_code:
-                new_lines.append("</pre>")
-                in_code = False
+        line = line.strip()
+        if line == "```":
+            if in_pre:
+                new_lines.append('</pre>')
+                in_pre = False
             else:
-                new_lines.append("<pre>")
-                in_code = True
+                new_lines.append('<pre>')
+                in_pre = True
             continue
-        if in_code:
+        if in_pre:
             new_lines.append(line)
             continue
-        if stripped == '':
+        if line == '':
             if in_paragraph:
-                new_lines.append("</p>")
+                new_lines.append('</p>')
                 in_paragraph = False
             else:
                 new_lines.append('')
             continue
-
         else:
-            if not in_paragraph and not stripped.startswith('#'):
-                new_lines.append("<p>")
+            if not in_paragraph and not line.startswith('#'):
+                new_lines.append('<p>')
                 in_paragraph = True
 
-        line = compile_headers(line)
-        line = compile_strikethrough(line)
-        line = compile_bold_stars(line)
-        line = compile_bold_underscore(line)
-        line = compile_italic_star(line)
-        line = compile_italic_underscore(line)
-        line = compile_code_inline(line)
-        line = compile_images(line)
-        line = compile_links(line)
+            line = compile_headers(line)
+            line = compile_strikethrough(line)
+            line = compile_bold_stars(line)
+            line = compile_bold_underscore(line)
+            line = compile_italic_star(line)
+            line = compile_italic_underscore(line)
+            line = compile_code_inline(line)
+            line = compile_images(line)
+            line = compile_links(line)
         new_lines.append(line)
     new_text = '\n'.join(new_lines)
-
     return new_text
+
+
+def markdown_to_html(markdown, add_css):
+    '''
+    Convert the input markdown into valid HTML,
+    optionally adding CSS formatting.
+
+    NOTE:
+    This function is separated out from the `compile_lines` function so that the doctests are much simpler.
+    In particular, by splitting these functions in two,
+    there's no need to add all of the HTML boilerplate code to the doctests in `compile_lines`.
+
+    NOTE:
+    The code for this function is simple enough that we don't even have a "real" doctest.
+    The only purpose of this doctest is to run the function and ensure that there are no errors.
+    The `assert` function prints no output whenever the input is "truthy".
+
+    >>> assert(markdown_to_html('this *is* a _test_', False))
+    >>> assert(markdown_to_html('this *is* a _test_', True))
+    '''
+
+    html = '''
+<html>
+<head>
+    <style>
+    ins { text-decoration: line-through; }
+    </style>
+    '''
+    if add_css:
+        html += '''
+<link rel="stylesheet" href="https://izbicki.me/css/code.css" />
+<link rel="stylesheet" href="https://izbicki.me/css/default.css" />
+        '''
+    html += '''
+</head>
+<body>
+    ''' + compile_lines(markdown) + '''
+</body>
+</html>
+    '''
+    return html
 
 
 def minify(html):
@@ -98,7 +143,7 @@ def minify(html):
     >>> minify('a\n\n\n\n\n\n\n\n\n\n\n\n\n\nb\n\n\n\n\n\n\n\n\n\n')
     'a b'
     '''
-    return html
+    return " ".join(html.split())
 
 
 def convert_file(input_file, add_css):
@@ -127,5 +172,5 @@ def convert_file(input_file, add_css):
     html = minify(html)
 
     # write the output file
-    with open(input_file[:-2]+'html', 'w') as f:
+    with open(input_file[:-2] + 'html', 'w') as f:
         f.write(html)
